5aa26aa5bba56a563f926dc6c73207ad8eb4a907
Otávio Silva <otaviosilva2632@gmail.com>
2022-01-03 19:12:31 -0300

    feat: refactoring insert

--- restapimongodb/src/main/java/com/otavio/restapimongodb/RestapimongodbApplication.java
+++ restapimongodb/src/main/java/com/otavio/restapimongodb/RestapimongodbApplication.java
@@ -38,20 +38,32 @@ public class RestapimongodbApplication {
 					BigDecimal.TEN,
 					LocalDateTime.now());
 
-			Query query = new Query();
-			query.addCriteria(Criteria.where("email").is(student.getEmail()));
+			// usingMongoTemplateAndQuery(repository, mongoTemplate, student);
 
-			List<Student> students = mongoTemplate.find(query, Student.class);
-
-			if (students.size() > 1)
-				throw new IllegalStateException("found many students with email" + student.getEmail());
-
-			if (students.isEmpty()) {
-				System.out.println("Inserting student " + student);
-				repository.insert(student);
-			} else {
-				System.out.println(student + " already exists");
-			}
+			repository.findStudentByEmail(student.getEmail())
+					.ifPresentOrElse(s -> {
+						System.out.println(student.getFirstName() + " " + student.getLastName() + " already exists");
+					}, () -> {
+						System.out.println("Inserting student " + student.getFirstName() + " " + student.getLastName());
+						repository.insert(student);
+					});
 		};
+	}
+
+	private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, Student student) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").is(student.getEmail()));
+
+		List<Student> students = mongoTemplate.find(query, Student.class);
+
+		if (students.size() > 1)
+			throw new IllegalStateException("found many students with email" + student.getEmail());
+
+		if (students.isEmpty()) {
+			System.out.println("Inserting student " + student.getFirstName() + " " + student.getLastName());
+			repository.insert(student);
+		} else {
+			System.out.println(student.getFirstName() + " " + student.getLastName() + " already exists");
+		}
 	}
 }
--- restapimongodb/src/main/java/com/otavio/restapimongodb/StudentRepository.java
+++ restapimongodb/src/main/java/com/otavio/restapimongodb/StudentRepository.java
@@ -1,7 +1,11 @@
 package com.otavio.restapimongodb;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface StudentRepository
     extends MongoRepository<Student, String> {
+
+  Optional<Student> findStudentByEmail(String email);
 }
