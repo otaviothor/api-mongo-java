bf2fa0c531686505f0d191d78996021f8dc097a3
Otávio Silva <otaviosilva2632@gmail.com>
2022-01-03 18:38:08 -0300

    feat: using queries

--- restapimongodb/src/main/java/com/otavio/restapimongodb/RestapimongodbApplication.java
+++ restapimongodb/src/main/java/com/otavio/restapimongodb/RestapimongodbApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 @SpringBootApplication
 public class RestapimongodbApplication {
@@ -16,7 +19,9 @@ public class RestapimongodbApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository) {
+	CommandLineRunner runner(
+			StudentRepository repository,
+			MongoTemplate mongoTemplate) {
 		return args -> {
 			Address address = new Address(
 					"Brazil",
@@ -33,7 +38,20 @@ public class RestapimongodbApplication {
 					BigDecimal.TEN,
 					LocalDateTime.now());
 
-			repository.insert(student);
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(student.getEmail()));
+
+			List<Student> students = mongoTemplate.find(query, Student.class);
+
+			if (students.size() > 1)
+				throw new IllegalStateException("found many students with email" + student.getEmail());
+
+			if (students.isEmpty()) {
+				System.out.println("Inserting student " + student);
+				repository.insert(student);
+			} else {
+				System.out.println(student + " already exists");
+			}
 		};
 	}
 }
