08e0303859b3e94a667ec96a0e61ab4a051ae58f
Otávio Silva <otaviosilva2632@gmail.com>
2022-01-03 16:34:23 -0300

    initial commit

---
+++ restapimongodb/src/main/java/com/otavio/restapimongodb/RestapimongodbApplication.java
@@ -0,0 +1,13 @@
+package com.otavio.restapimongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapimongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapimongodbApplication.class, args);
+	}
+
+}
