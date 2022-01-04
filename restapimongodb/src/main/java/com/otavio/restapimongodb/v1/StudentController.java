1f476752f9c688a3be6ac9f954edbe29bc2e726a
Otávio Silva <otaviosilva2632@gmail.com>
2022-01-03 23:23:18 -0300

    feat: organize imports

--- restapimongodb/src/main/java/com/otavio/restapimongodb/v1/StudentController.java
+++ restapimongodb/src/main/java/com/otavio/restapimongodb/v1/StudentController.java
@@ -7,6 +7,7 @@ import com.otavio.restapimongodb.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import lombok.AllArgsConstructor;
 
 @RestController
